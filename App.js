e1c2dd1d3c8ac0869a16aa6b7b7de598ff9fef12
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-11-29 14:14:34 +0900

    [Add] 백드롭 추가.

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (
-    <View>
+    <View style={styles.backdrop}>
       <Text>Hello, World!!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  backdrop: {
+    flex: 1,
+    backgroundColor: "rgb(241, 231, 223)",
+  },
+});
 
 export default App;
